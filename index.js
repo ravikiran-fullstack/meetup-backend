5f51f4186ba328316ab709f9bbf8436e5cdfc1d7
Ravikira Yadava <ravikiranjobs21@gmail.com>
2023-12-09 22:03:55 -0500

    added basic express server with cors and api

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const express = require('express');
+const cors = require('cors');
+const meetupsList = require('./model/meetupsList');
+
+const app = express();
+const port = 4000;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/api/meetups',(req, res) => {
+    res.json(meetupsList);
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});

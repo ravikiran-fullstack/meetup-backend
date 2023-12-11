26540a5912fb37dbf68df6fe0f37d1fe236969a9
Ravikira Yadava <ravikiranjobs21@gmail.com>
2023-12-10 21:18:24 -0500

    Created flow for GET and POST methods between NodeJS and MongoDB

--- index.js
+++ index.js
@@ -1,25 +1,81 @@
+const fs = require('fs');
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
-const meetupsList = require('./model/meetupsList');
-
+const { v4: uuidv4 } = require('uuid');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+
 const port = 4000;
 
+const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.f1oomps.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri);
+
+client.connect(function (err) {
+  if (err) console.log(err);
+
+  console.log('Connected successfully to server');
+
+  const db = client.db('meetupsData');
+  const collection = db.collection('meetups');
+
+  // Find some documents
+  collection.find({}).toArray(function (err, docs) {
+    if (err) console.log(err);
+
+    console.log('Found the following records');
+    console.log('docs', docs);
+  });
+
+  client.close();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
-app.use(cors());
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/api/meetups',(req, res) => {
-    res.json(meetupsList);
+app.get('/api/meetups', async (req, res) => {
+  console.log('api/meetups data requested');
+
+  const result = await client
+    .db('meetupsData')
+    .collection('meetups')
+    .find({})
+    .toArray();
+
+  res.json(result);
 });
 
+app.post('/api/meetups', async (req, res) => {
+  console.log('api/meetups data posted');
+  const meetup = req.body;
+  if (!meetup) {
+    return res.status(400).json({ message: 'No meetup data provided' });
+  }
+
+  const id = uuidv4();
+  const newMeetup = { ...meetup, id };
+
+  try {
+    await client.db('meetupsData').collection('meetups').insertOne(newMeetup);
+    return res.json(newMeetup);
+  } catch (error) {
+    return res.status(500).json({ message: 'Server Error occurred DB error' });
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
